1b97a43242bd42a51ed86f77b0d6e8bc8078b2d9
Peter Culak <peter.culak@ymail.com>
2014-11-30 22:50:44 +0000

    Victoria status

--- tfl-get.php
+++ tfl-get.php
@@ -34,13 +34,36 @@ class Tfl
                 $regexp = "<td\s[^>]*>(.*)<\/td>";
 
                 if (preg_match_all("/$regexp/siU", $row, $matches, PREG_SET_ORDER)) {
-                    print $matches[1][1] . ' - ' . $matches[2][1] . "\n";
+                    echo $matches[1][1] . ' - ' . $matches[2][1] . "\n";
                 }
             }
         }
     }
-}
 
+    public static function printTubeStauts()
+    {
+        $data = file_get_contents('http://www.tfl.gov.uk/tube-dlr-overground/status/');
+        $data = str_replace("\n", '', $data);
+        $data = str_replace("\r", '', $data);
+//        var_dump($data);die;
+        preg_match_all("/rainbow-list-item victoria.*?\/li\>/im", $data, $matches);
+        if (!$matches[0][0]) {
+            throw new \Exception("Probably regexp needs changing here");
+        }
+
+        preg_match_all("/disruption-summary.*?\/span\>/", $matches[0][0], $m);
+        if (!$m[0][0]) {
+            throw new \Exception("Probably regexp needs changing here");
+        }
+        preg_match("/span\>(.*)\<\/span\>/", $m[0][0], $x);
+        if (!$x[0][1]) {
+            throw new \Exception("Probably regexp needs changing here");
+        }
+        echo 'Victoria Line: ' . str_replace(['  ', '<br />'], '', $x[1]) . "\n";
+    }
+}
+Tfl::printTubeStauts();
+echo "\n";
 $destination = 1000248;
 $origin = 1001555;
 
