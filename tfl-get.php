a9139f333aeb71e39775b27827cc011eca1ab360
Peter Culak <peter.culak@ymail.com>
2014-12-09 10:48:22 +0000

    Adding new tfl url

--- tfl-get.php
+++ tfl-get.php
@@ -24,8 +24,9 @@ class Tfl
         $hour = $date->format('H');
         $minute = $date->format('i');
         $date = $date->format("Ymd");
-
-        $data = file_get_contents("http://journeyplanner.tfl.gov.uk/user/XSLT_TRIP_REQUEST2?language=en&execInst=&sessionID=0&ptOptionsActive=1&itOptionsActive=1&imparedOptionsActive=1&ptAdvancedOptions=1&place_origin=London&place_destination=London&show_origin=Victoria&show_destination=mitcham+eastfields&type_origin=stop&type_destination=stop&itdTripDateTimeDepArr=dep&datepicker=Today&stepfree-access=no-requirements&routeType=LEASTTIME&includedMeans=checkbox&inclMOT_2=on&inclMOT_1=on&inclMOT_5=on&inclMOT_0=on&inclMOT_4=on&inclMOT_9=on&inclMOT_7=on&inclMOT_8=on&inclMOT_3=on&trITMOTvalue101=60&trITMOTvalue=20&trITMOT=100&changeSpeed=normal&name_origin=$origin&name_destination=$destination&itdDate=$date&itdTimeHour=$hour&itdTimeMinute=$minute");
+        //http://tfl.gov.uk/plan-a-journey/results?IsAsync=False&JpType=publictransport&From=Victoria+%28London%29%2c+Victoria&FromGeolocation=&ToGeolocation=&ViaGeolocation=&To=Mitcham%2c+Mitcham+Eastfields&TimeIs=departing&Date=20141209&Time=1100&Via=&NationalSearch=false&JourneyPreference=leasttime&Mode=tube&Mode=bus&Mode=dlr&Mode=river-bus&Mode=tram&Mode=cable-car&Mode=overground&Mode=national-rail&Mode=coach&AccessibilityPreference=norequirements&WalkingSpeed=average&MaxWalkingMinutes=40&CyclePreference=AllTheWay&SavePreferences=false&FromId=1000248&ToId=1001555
+        $url = "http://journeyplanner.tfl.gov.uk/user/XSLT_TRIP_REQUEST2?language=en&execInst=&sessionID=0&ptOptionsActive=1&itOptionsActive=1&imparedOptionsActive=1&ptAdvancedOptions=1&place_origin=London&place_destination=London&show_origin=Victoria&show_destination=mitcham+eastfields&type_origin=stop&type_destination=stop&itdTripDateTimeDepArr=dep&datepicker=Today&stepfree-access=no-requirements&routeType=LEASTTIME&includedMeans=checkbox&inclMOT_2=on&inclMOT_1=on&inclMOT_5=on&inclMOT_0=on&inclMOT_4=on&inclMOT_9=on&inclMOT_7=on&inclMOT_8=on&inclMOT_3=on&trITMOTvalue101=60&trITMOTvalue=20&trITMOT=100&changeSpeed=normal&name_origin=$origin&name_destination=$destination&itdDate=$date&itdTimeHour=$hour&itdTimeMinute=$minute";
+        $data = file_get_contents($url);
 
         preg_match_all("/tr class=\"det_hov.*?\/tr\>/", $data, $m);
         if (is_array($m)) {
